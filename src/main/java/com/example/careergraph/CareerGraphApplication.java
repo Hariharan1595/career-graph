0d0838d30ef100cd17fac07b38e41c8c84b55ce2
Hariharan <hariharan.r1810@gmail.com>
2026-08-18 10:30:19 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/careergraph/CareerGraphApplication.java
@@ -0,0 +1,13 @@
+package com.example.careergraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerGraphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/careergraph/CareerGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.careergraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
