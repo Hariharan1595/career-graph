5af279ec35e508c61f4b01ba316923fa8bcbed40
Hariharan <132120282+Hariharan1595@users.noreply.github.com>
2026-08-18 14:37:43 +0530

    Update allowed origins in CorsConfigu

--- src/main/java/com/example/careergraph/config/CorsConfig.java
+++ src/main/java/com/example/careergraph/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://career-graph-xigf.vercel.app")
                 .allowedMethods(
                         "GET",
                         "POST",
